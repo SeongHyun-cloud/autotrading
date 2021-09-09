82f277260a942860d9afa2eb696508b6cffa8205
SeongHyun-cloud <shparkwk@gmail.com>
2021-09-09 16:54:48 +0900

    rc_trade v1.9

--- rc_trading/strategy.py
+++ rc_trading/strategy.py
@@ -9,6 +9,7 @@ class RCstrategy:
         self.purchaseAmount = 0
         self.data = {}
         self.strategy_num = strategy
+        self.last_follow_purchase = 0
     ##
     # def myStrategy
     #   taking current price of coin buys if it is reasonable
@@ -21,8 +22,8 @@ class RCstrategy:
         buy_factor = factor
         self.priceQ.popleft()
 
-        percent_of_deposit = 4
-        leverage = 2
+        percent_of_deposit = 6
+        leverage = 4
         fee_percent = 0.0002 * leverage
 
 
@@ -35,32 +36,39 @@ class RCstrategy:
             self.lastPrice = value
             return True
 
+
+
+        
         #factor calculation Buying condition due to drammatic price drop
-        if(self.lastPrice != -1) :
+        if self.lastPrice != -1 :
             profit_or_loss = value / self.lastPrice
             profit_or_loss = abs(profit_or_loss)
-            if profit_or_loss >= 1.1:
-                sell_factor += 1
-            elif profit_or_loss > 1.04:
-                sell_factor += 0.8
-            elif profit_or_loss > 1.025:
-                sell_factor += 0.5
-            elif profit_or_loss > 1.01:
-                sell_factor += 0.2
-            elif profit_or_loss > 0.98:
-                buy_factor += 0.2
-            elif profit_or_loss > 0.94:
-                buy_factor += 0.5
-            elif profit_or_loss > 0.91:
-                buy_factor += 0.8
-            elif profit_or_loss > 0.89:
-                buy_factor += 1
-            
-        
+            profit_or_loss_buy = profit_or_loss
+            if self.last_follow_purchase != 0:
+                profit_or_loss_buy = abs(value / self.last_follow_purchase) + 0.05
+            if profit_or_loss >= 1:
 
+                if profit_or_loss >= 1.1:
+                    sell_factor += 1
+                elif profit_or_loss > 1.04:
+                    sell_factor += 0.8
+                elif profit_or_loss > 1.025:
+                    sell_factor += 0.5
+                elif profit_or_loss > 1.01:
+                    sell_factor += 0.2
+            else: 
+                if profit_or_loss_buy > 0.98:
+                    buy_factor += 0.2
+                elif profit_or_loss_buy > 0.94:
+                    buy_factor += 0.5
+                elif profit_or_loss_buy > 0.91:
+                    buy_factor += 0.8
+                elif profit_or_loss_buy > 0.89:
+                    buy_factor += 1
+            
         if status == "OVERSOLD" and self.deposit > 0 and buy_factor >= 1:
             if self.purchaseAmount >= self.deposit:
-                #print("buy Happens1")
+                
                 #making average price
                 acoin = self.deposit / value
                 self.lastPrice = ((self.lastPrice * self.coin) + (value * acoin)) /(self.coin + acoin)
@@ -68,6 +76,7 @@ class RCstrategy:
                 fee = self.coin * value * fee_percent
                 self.coin -= fee/value
                 self.deposit = 0
+                self.last_follow_purchase = value
                 return True
             else:
                 #print("buy Happens2")
@@ -76,6 +85,7 @@ class RCstrategy:
                 self.coin += acoin
                 fee = self.coin * value * fee_percent
                 self.deposit -= self.purchaseAmount - fee
+                self.last_follow_purchase = value
                 return True
 
 
@@ -89,6 +99,7 @@ class RCstrategy:
                 self.lastPrice = -1
                 self.purchaseAmount = 0
                 self.coin = 0
+                self.last_follow_purchase = 0
                 return True
         elif self.strategy_num == 2:
             if status == "OVERBOUGHT" and self.coin > 0 and sell_factor >= 1.4:
@@ -100,6 +111,7 @@ class RCstrategy:
                 self.lastPrice = -1
                 self.purchaseAmount = 0
                 self.coin = 0
+                self.last_follow_purchase = 0
                 return True
             elif status == "OVERBOUGHT" and self.coin > 0.0001 and sell_factor >= 1:
                 
@@ -120,11 +132,14 @@ class RCstrategy:
         def liquidation_calculator():
             profit = self.coin * value - self.lastPrice * self.coin
             profit *= leverage
-            total = self.deposit + self.coin*value - profit
+            total = self.deposit + self.coin * self.lastPrice + profit
             return total
 
 
-        if liquidation_calculator() < 0 :
+        if liquidation_calculator() < 0.1 :
+            print(value, self.deposit, self.coin, self.lastPrice)
+            print(self.deposit + self.coin* self.lastPrice )
+            print(self.coin * value - self.lastPrice * self.coin)
             input("LIQUIDAITON OCCURED")
         return False
         
